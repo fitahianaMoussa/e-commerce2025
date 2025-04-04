0ab6e7463ccc5058f5699c59ff700237c90375b2
fitahianaMoussa <fitahianamoussa3@gmail.com>
2025-04-04 15:49:14 +0400

    implementing the repository method

--- Infrastructure/Repositories/ProductRepository.cs
+++ Infrastructure/Repositories/ProductRepository.cs
@@ -1,42 +1,44 @@
 using Core.Entities;
 using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Repositories;
 
-public class ProductRepository: IProductRepository
+public class ProductRepository(StoreContext context) : IProductRepository
 {
-    public Task<Product?> GetProductByIdAsync(int id)
+    public async Task<Product?> GetProductByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return await context.Products.FindAsync(id);
     }
 
-    public Task<IReadOnlyCollection<Product>> GetProductsAsync()
+    public async Task<IReadOnlyCollection<Product>> GetProductsAsync()
     {
-        throw new NotImplementedException();
+        return await context.Products.ToListAsync();
     }
 
     public void AddProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Products.Add(product);
     }
 
     public void UpdateProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Entry(product).State = EntityState.Modified;
     }
 
     public void DeleteProduct(Product product)
     {
-        throw new NotImplementedException();
+        context.Products.Remove(product);
     }
 
     public bool ProductExists(int id)
     {
-        throw new NotImplementedException();
+        return context.Products.Any(e => e.Id == id);
     }
 
-    public Task<bool> SaveChangesAsync()
+    public async Task<bool> SaveChangesAsync()
     {
-        throw new NotImplementedException();
+        return await context.SaveChangesAsync() > 0;
     }
 }
